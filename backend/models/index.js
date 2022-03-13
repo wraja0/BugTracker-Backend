87491276995c49b6e69e3e7168679f19e8386794
wraja899 <93971144+wraja899@users.noreply.github.com>
2022-03-13 06:52:34 -0400

    Delete backend directory

--- backend/models/index.js
+++
@@ -1,9 +0,0 @@
-require('../config/db.connection');
-
-module.exports = {
-    User: require('./userModel'),
-    AllBugs: require('./allBugsModel'),
-    Comments: require('./commentsModel'),
-    Tests: require('./testsModel'),
-    BugVH: require('./bugVhModel')
-}
