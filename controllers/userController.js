15a0744e964981c504decada856951315a694fd5
Wajeah Raja <rajawajeah@gmail.com>
2022-03-13 19:34:27 -0400

    heroku deploy #3

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,7 +7,7 @@ function authenticateToken(req, res, next) {
   // Read the JWT access token from the request header
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  if (token == null) return res.sendStatus(401); // Return 401 if no token
+  if (token == null) return res.send('dickrider gtfoh'); // Return 401 if no token
 
   // Verify the token using the Userfront public key
   jwt.verify(token, process.env.USERFRONT_PUBLIC_KEY, (err, auth) => {
