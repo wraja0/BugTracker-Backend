d20007e7802c16078c3d940d302e5ffc42224ef7
Wajeah Raja <rajawajeah@gmail.com>
2022-03-13 19:23:00 -0400

    adding jwt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/userModel');
+const jwt = require("jsonwebtoken");
+
+function authenticateToken(req, res, next) {
+  // Read the JWT access token from the request header
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401); // Return 401 if no token
+
+  // Verify the token using the Userfront public key
+  jwt.verify(token, process.env.USERFRONT_PUBLIC_KEY, (err, auth) => {
+    if (err) return res.sendStatus(403); // Return 403 if there is an error verifying
+    req.auth = auth;
+    next();
+  });
+}
 
 // QuerybyUserID handler
+router.use(authenticateToken)
+
 router.get('/:query', async (req, res)=> {
     try {
         const userQued = await User.findById(req.params.query);
